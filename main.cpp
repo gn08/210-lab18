93eae39af6ab677a58a762c1491cd21cae902788
gn08 <gabbyfilipova@gmail.com>
2024-10-05 16:05:49 -0700

    commit 4:05

--- main.cpp
+++ main.cpp
@@ -1,14 +1,23 @@
 #include <iostream>
+#include <string>
 using namespace std;
 
 struct Review{
-
+    double rating;
+    string comments;
 }
 
-void add_at_head()
-void add_at_tail()
-void display_review()
+void add_at_head(Review*& head, Review*& tail, double rating, const string& );
+void add_at_tail(Review*& head, Review*& tail, double rating, const string& );
+void display_review(Review*& head);
 
 int main() {
-    
+    Review* head = nullptr;
+    Review* tail = nullptr;
+
+    cout << "Which linked list method should we use?" << endl;
+    cout << "[1] New nodes are added at the head of the linked list";
+    cout << "[2] New nodes are added at the tail of the linked list";
+    cout << "Choice: ";
+    cin >> choice;
 }
